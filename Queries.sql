55ac469294e8f77d2a1b3e356a9812c608c93595
Gina Yang <yichinyang2024@gmail.com>
2026-05-27 19:15:19 +0100

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,5 +1,29 @@
 /*
 CREATED BY: Gina Yang
+CREATE DATE: 2026/05/27
+DESCRIPTION: Check duplicate entries
+*/
+
+SELECT
+    *,
+    COUNT(*) AS duplicate_count
+FROM orders
+GROUP BY
+    "Order ID",
+    "order date",
+    "customer_name",
+	"Product ID",
+	category,
+    country,
+    sales,
+    profit,
+    quantity,
+	"shipping cost"
+HAVING COUNT(*) > 1;
+
+	
+/*
+CREATED BY: Gina Yang
 CREATE DATE: 2026/05/26
 DESCRIPTION: Check whether data is imported successfully
 */
@@ -8,7 +32,7 @@ SELECT
 	*
 FROM
 	Orders
-LIMIT 5
+LIMIT 5;
 
 
 /*
@@ -54,17 +78,26 @@ ORDER BY SUM(sales) DESC
 /*
 CREATED BY: Gina Yang
 CREATE DATE: 2026/05/26
-DESCRIPTION: Sales, profit, profit margin by country
+DESCRIPTION: Sales, profit, profit margin by country & label margin
 */
 
 SELECT
     Country,   
     ROUND(SUM(sales) / 1000.0, 2) || 'K' AS total_sales_k,
     ROUND(SUM(profit) / 1000.0, 2) || 'K' AS total_profit_k,
-	ROUND(SUM(profit) * 100.0 / SUM(sales), 2) AS profit_margin
+	ROUND(SUM(profit) * 100.0 / SUM(sales), 2) AS profit_margin,
+	CASE
+        WHEN SUM(profit) * 100.0 / SUM(sales) < 0
+        THEN 'Loss'
+        WHEN SUM(profit) * 100.0 / SUM(sales) < 10
+        THEN 'Low Margin'
+        WHEN SUM(profit) * 100.0 / SUM(sales) < 20
+        THEN 'Medium Margin'
+        ELSE 'High Margin'
+    END AS margin_category
 FROM orders
 GROUP BY Country
-ORDER BY profit_margin DESC	
+ORDER BY profit_margin DESC;
 
 
 /*
@@ -99,6 +132,22 @@ FROM orders
 GROUP BY City
 ORDER BY total_profit_k DESC
 LIMIT 10
+
+
+/*
+CREATED BY: Gina Yang
+CREATE DATE: 2026/05/26
+DESCRIPTION: Profit by ship mode
+*/
+
+SELECT
+    "Ship mode",   
+    ROUND(SUM(sales) / 1000.0, 2) AS total_sales_k,
+    ROUND(SUM(profit) / 1000.0, 2) AS total_profit_k,
+	ROUND(SUM(profit) * 100.0 / SUM(sales), 2) || '%' AS profit_margin
+FROM orders
+GROUP BY "Ship mode"
+ORDER BY total_profit_k DESC
 
 	
 /*
@@ -235,3 +284,29 @@ SELECT
 FROM Cleaned_Dates
 GROUP BY order_month
 ORDER BY order_month
+
+
+/*
+CREATED BY: Gina Yang
+CREATE DATE: 2026/05/27
+DESCRIPTION: Negative profit margin products
+*/
+
+SELECT
+    "product name",
+    ROUND(SUM(sales), 2) AS total_sales,
+    ROUND(SUM(profit), 2) AS total_profit,
+    ROUND(SUM(profit) * 100.0 / SUM(sales), 2)
+    AS profit_margin
+FROM orders
+GROUP BY "product name"
+HAVING profit_margin < 0
+ORDER BY total_sales DESC;
+
+
+/*
+CREATED BY: Gina Yang
+CREATE DATE: 2026/05/27
+DESCRIPTION: Label profit margin category
+*/
+
